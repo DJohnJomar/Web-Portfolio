50c2709d93f10700a6f9ff5f2f037a24c3d4c755
John Jomar Dimaunahan <johndjomar@gmail.com>
2025-04-28 11:20:19 +0800

    transitions complete, adjusted some minor thigs

---
+++ script.js
@@ -0,0 +1,15 @@
+// let projects = document.querySelectorAll('.project');
+
+// projects.forEach(function (project){
+//     let paragraph = project.querySelector('p');
+//     let image = project.querySelector('img');
+//     project.addEventListener('mouseover', function(){
+//         paragraph.hidden = false;
+//         image.style.opacity = '0.5';
+//     });
+//     project.addEventListener('mouseout', function(){
+//         paragraph.hidden = true;
+//         image.style.opacity = '1';
+//     })
+// });
+
